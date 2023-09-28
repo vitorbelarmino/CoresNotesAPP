fe3830811f1f62e61bb8efd9d146f0a842e83b77
vitorbelarmino <vitor.belarmino@hotmail.com>
2023-09-28 00:04:09 -0300

    fix: responsive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,11 +6,11 @@ import closeIcon from './../assets/close-icon.png'
 export default function Header() {
   return (
     <header>
-      <div className='flex justify-between items-center px-10 py-2 bg-white' >
-        <div className='flex gap-5 items-center w-[100%]' >
-          <Image src={noteIcon} alt="note icon" width={36} />
-          <p>CoreNotes</p>
-          <div className='flex items-center border rounded-[3px] border-[#D9D9D9] shadow-md h-7 bg-white ml-2 w-[100%] sx:[314px] md:w-[530px] min-w-[140px]'>
+      <div className='flex justify-between items-center px-6 py-2 bg-white' >
+        <div className='flex gap-3 items-center w-[100%] mr-2' >
+          <Image src={noteIcon} alt="note icon" width={29} />
+          <p className='text-sm'>CoreNotes</p>
+          <div className='flex items-center border rounded-[3px] border-[#D9D9D9] h-7 bg-white ml-2 w-[314px] md:w-[530px] min-w-[140px] drop-shadow-[1px_1px_3px_rgba(0,0,0,0.25)]'>
             <input type="text" className='h-3 pl-2 text-xs focus:outline-none w-[100%]' placeholder='Pesquisar notas' />
             <span className='mr-2'>
               <Image src={searchIcon} alt="search" width={13} />
